f13eb39e502eaa6983b46fabefe5c3065a3f0f2b
King-andu21 <apook@hkhk.edu.ee>
2026-05-04 10:22:20 +0300

    Create Ülesanne1.php

---
+++ Ülesanne1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $test_var = "testing";
+    echo $test_var;
+
+    echo "(\(\";
+    ?>
+</body>
+</html>
